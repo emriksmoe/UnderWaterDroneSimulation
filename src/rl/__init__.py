a346afcfd81646060fc0d60188a5740241efd8d0
emriksmoe <emriksmoe@gmail.com>
2025-11-21 13:06:15 +0900

    fix: Deleted anything to do with multiple agent RL

--- src/rl/__init__.py
+++ src/rl/__init__.py
@@ -2,10 +2,8 @@
 # Make key classes easily importable
 from .state_manager import RLStateManager
 from .environments.single_agent_env import DTNDroneEnvironment
-#from .environments.multi_agent_env import IndeoendentMultiAgentDTNEnvironment
 
 __all__ = [
     'RLStateManager',
     'DTNDroneEnvironment', 
-  #  'MultiAgentDTNEnvironment'
 ]
--- src/rl/environments/__init__.py
+++ src/rl/environments/__init__.py
@@ -1,9 +1,8 @@
 """RL environments for DTN drone training"""
 
 from .single_agent_env import DTNDroneEnvironment
-#from .multi_agent_env import MultiAgentDTNEnvironment
+
 
 __all__ = [
     'DTNDroneEnvironment',
-  #  'MultiAgentDTNEnvironment'
 ]
